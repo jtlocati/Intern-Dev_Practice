eedb6ed3e503c5489e68d8f16b0a95f358cf713d
jet locati <jetlocati4922@gmail.com>
2026-07-17 07:57:43 -0600

    Update error.tsx

--- bug-dash/app/error.tsx
+++ bug-dash/app/error.tsx
@@ -1,7 +1,10 @@
-export default function App() {
+'use client';
+//MUST IMPLEMENT - currently silenceing error
+
+/*export default function App() {
   return (
     <div>
       <h1>App</h1>
     </div>
   );
-}
+}*/
